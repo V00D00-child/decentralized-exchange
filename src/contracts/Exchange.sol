c6f7466abaa01d46ce1633fd15bcdd3eb5add210
Idris Bowman <corndog2@Idriss-MacBook-Air.local>
2021-07-10 19:36:30 -0400

    allow user to make an order

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -28,7 +28,7 @@
 // [x] Deposit tokens
 // [x] Withdraw tokens
 // [x] Check blances
-// [] Make order
+// [x] Make order
 // [] Cancel order
 // [] Fill order
 // [] Charge fees
@@ -45,10 +45,32 @@ contract Exchange {
     uint256 public feePercent; 
     address constant ETHER = address(0); // store ether in tokens mapping with blank address
     mapping(address => mapping(address => uint256)) public tokens; // {tokenAddress -> userAddress-> amount}
-    
+    mapping(uint256 => _Order) public orders; // store all user orders
+    uint256 public orderCount; // counter cache
+
     // Events
     event Deposit(address token, address user, uint256 amount, uint256 balance);
-    event Withdraw(address token, address user, uint amount, uint balance);
+    event Withdraw(address token, address user, uint256 amount, uint256 balance);
+    event Order(
+        uint256 id,
+        address user,
+        address tokenGet,
+        uint256 amountGet,
+        address tokenGive,
+        uint256 amountGive,
+        uint256 timestamp
+    );
+
+    // Structs
+    struct _Order {
+        uint256 id;
+        address user;
+        address tokenGet;
+        uint256 amountGet;
+        address tokenGive;
+        uint256 amountGive;
+        uint256 timestamp;
+    }
 
     constructor(address _feeAcount, uint256 _feePercent) public{
         feeAccount = _feeAcount;
@@ -65,14 +87,14 @@ contract Exchange {
         emit Deposit(ETHER, msg.sender, msg.value, tokens[ETHER][msg.sender]);
     }
 
-    function withdrawEther(uint _amount) payable public {
+    function withdrawEther(uint256 _amount) payable public {
         require(tokens[ETHER][msg.sender] >= _amount);
         tokens[ETHER][msg.sender] = tokens[ETHER][msg.sender].sub(_amount);
         msg.sender.transfer(_amount);
         emit Withdraw(ETHER, msg.sender, _amount, tokens[ETHER][msg.sender]);
     }
 
-    function depositToken(address _token, uint _amount) public {
+    function depositToken(address _token, uint256 _amount) public {
         // Don't allow Ether deposits
         require(_token != ETHER);
 
@@ -86,7 +108,7 @@ contract Exchange {
         emit Deposit(_token, msg.sender, _amount, tokens[_token][msg.sender]);
     }
 
-    function withdrawToken(address _token, uint _amount) public {
+    function withdrawToken(address _token, uint256 _amount) public {
         require(_token != ETHER);
         require(tokens[_token][msg.sender] >= _amount);
         tokens[_token][msg.sender] = tokens[_token][msg.sender].sub(_amount);
@@ -96,5 +118,11 @@ contract Exchange {
 
     function balanceOf(address _token, address _user) public view returns (uint256) {
         return tokens[_token][_user];
+    }
+
+    function makeOrder(address _tokenGet, uint256 _amountGet, address _tokenGive, uint256 _amountGive) public {
+        orderCount = orderCount.add(1);
+        orders[orderCount] = _Order(orderCount, msg.sender, _tokenGet, _amountGet, _tokenGive, _amountGive, now);
+        emit Order(orderCount, msg.sender, _tokenGet, _amountGet, _tokenGive, _amountGive, now);
     }
 }
