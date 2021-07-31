f74e722da3f05a7091ff896ad768a8076747f6f9
Idris Bowman <corndog2@Idriss-MacBook-Air.local>
2021-07-31 15:21:52 -0400

    add config for kovan deploymemt

--- src/components/App.js
+++ src/components/App.js
@@ -70,7 +70,41 @@ class App extends Component {
       <div>
         <Navbar disConnectWallet = {this.disConnectWallet} />
         <Connection connectWallet = {this.connectWallet} />
-        {this.props.contractsLoaded ? <Content /> : <div className=""><h1 className="intro-text text-center">Welcome please connect your wallet</h1></div>}
+        {this.props.contractsLoaded ? <Content /> : 
+        <div className="p-5">
+          <h1 className="intro-text text-center">This is a crypto currency exchange powered by smart contracts</h1>
+          <p className="text-center small">(All transaction on this DApp takes place on Kovan test network. This allows you to intract with
+            the app without spending real Ether)
+          </p>
+          <hr></hr>
+          <p className="text-left">Exchange smart contract(Kovan Test Network):<a target="_blank" rel="noopener noreferrer" href="https://kovan.etherscan.io/">https://kovan.etherscan.io/</a></p>
+          <p className="text-left">DollHair ERC-20 token smart contract(Kovan Test Network):<a target="_blank" rel="noopener noreferrer" href="https://kovan.etherscan.io/">https://kovan.etherscan.io/</a></p>
+          <p>Author: Idris Bowman</p>
+          <ul>
+            <li>Website: <a target="_blank" rel="noopener noreferrer" href="https://idrisbowman.com">https://idrisbowman.com</a></li>
+            <li>Github: <a target="_blank" rel="noopener noreferrer" href="https://github.com/dris1995 ">https://github.com/dris1995 </a></li>
+          </ul>
+          <hr></hr>
+          <h3  className="text-left">What can you do</h3>
+          <ul>
+            <li>Trade Ether for DollHair token, or DollHair token for Ether(real-time)</li>
+            <li>Deposit Ether</li>
+            <li>Deposit DollHair token</li>
+            <li>Make buy/sell orders</li>
+            <li>Fill open buy/sell order</li>
+            <li>Price charts</li>
+          </ul>
+          <hr></hr>
+          <h3  className="intro-text text-left">Technologyies</h3>
+          <ul>
+            <li>React</li>
+            <li>Redux</li>
+            <li>Ethereum</li>
+            <li>Solidity</li>
+            <li>Metamask</li>
+            <li>ERC-20</li>
+          </ul>
+        </div>}
       </div>
     );
   }
--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,8 @@
 require('babel-register');
 require('babel-polyfill');
 require('dotenv').config();
+const HDWalletProvider = require('truffle-hdwallet-provider-privkey');
+const privatekeys = process.env.PRIVATE_KEYS || "";
 
 module.exports = {
   networks: {
@@ -8,6 +10,19 @@ module.exports = {
      host: "127.0.0.1", 
      port: 7545,
      network_id: "*",
+    },
+    kovan: {
+      provider: function() {
+        return new HDWalletProvider(
+          // Private key
+          privatekeys.split(','), // Array of account private keys
+          // Url to an Ethereum Node
+          `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}` // Url to an Ethereum Node
+        )
+      },
+      gas: 5000000,
+      gasPrice: 25000000000,
+      network_id: 42
     },
   },
   contracts_directory: './src/contracts/',
